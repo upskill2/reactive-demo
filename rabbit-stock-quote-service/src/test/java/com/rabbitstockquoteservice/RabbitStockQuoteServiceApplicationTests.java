fdb036449fcfa145515bf2b69d1ed253540afbf8
taras.chmeruk@gameloft.com <taras.chmeruk@gameloft.com>
2023-07-20 09:27:34 +0300

    rabbitmq new project

---
+++ rabbit-stock-quote-service/src/test/java/com/rabbitstockquoteservice/RabbitStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
